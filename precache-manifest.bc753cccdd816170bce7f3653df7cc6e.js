f7d650e38d239071589daa55611a48726ad28109
zekpause <samson6603@gmail.com>
2022-02-04 15:55:20 +0800

    Updates

---
+++ precache-manifest.bc753cccdd816170bce7f3653df7cc6e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224641a632c5077448a309f95468d22c",
+    "url": "/robotfriendhook/index.html"
+  },
+  {
+    "revision": "b2f11bd2b706cc658fdd",
+    "url": "/robotfriendhook/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "9bb94d4b6afa9ddc026f",
+    "url": "/robotfriendhook/static/css/main.25850605.chunk.css"
+  },
+  {
+    "revision": "b2f11bd2b706cc658fdd",
+    "url": "/robotfriendhook/static/js/2.f3376ae2.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/robotfriendhook/static/js/2.f3376ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd09c4fa8b3d5457921",
+    "url": "/robotfriendhook/static/js/3.ba14fdb8.chunk.js"
+  },
+  {
+    "revision": "9bb94d4b6afa9ddc026f",
+    "url": "/robotfriendhook/static/js/main.ad065fd9.chunk.js"
+  },
+  {
+    "revision": "532f7ef1ae4b1b930a73",
+    "url": "/robotfriendhook/static/js/runtime-main.c74c3b65.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriendhook/static/media/SEGA.12a3db8b.woff"
+  }
+]);
